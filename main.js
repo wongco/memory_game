98c986454632582c2dc8fe84d33f984a86bad9d9
wongco <ginson@gmail.com>
2018-10-27 20:48:11 -0700

    convert program to work off object model

--- main.js
+++ main.js
@@ -1,67 +1,144 @@
-// data strucure of memory game
+// Data Structure of Game
 // each cell in row/col grid represents a card object that holds two properties
 // card object data
 //   matchValue:unique ID shared with another card
 //   matchedStatus: boolean if matching partner has been discovered
 
+// Intro Screen
+// start game, view high scores = top5 only.
+//   order by least amount of clicks
+
+// New Game Setup
 // game process - once started
 // display grid to DOM
-// increase counter to track every click on a card that has matched: false
-// show card and if variable findPair is true
-//
+
+// Turn Based Listener
+// utilize listener for any card click
+// if selected card matchstatus is true, ignore click
+// else
+//   reveal card
+//   increase totalCardCheckCount;
+//   add card to checking cardmatch queue
+//   if card match queue lengh is 2, compare
+//     if matching, empty queue, add 2 to count of totalmatched cards.
+//     else flip both card back over
+// check for victory condition
+//   if totalmatched cards === totalCards
+//     game is over
+//     kill eventlistener
+//     save score to LocalStorage
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Wait for DOM Load Content prior to executing;
-  // use grid layout to store information
+  var gameContainer = document.getElementById('game');
+  gameContainer.addEventListener('click', function(event) {
+    console.log(event);
+    // find element being targeted
+    // send command to game.activateTurn(id);
+  });
 });
 
-function createGameGrid(rows, cols) {
-  try {
-    // if rows * cols is not even number, throw error
-    if ((rows * cols) % 2 === 1) {
-      throw 'Total cards is an odd number!';
-    }
-    var gameGrid = [];
-    var matchValues = createMatchValues(rows * cols);
-    for (var i = 0; i < rows; i++) {
-      var row = [];
-      for (var j = 0; j < cols; j++) {
-        var randomResultIdx = Math.floor(Math.random() * matchValues.length);
-        row.push({
-          matchValue: matchValues.splice(randomResultIdx, 1)[0],
-          matched: false
-        });
+var game = {
+  gameGrid: [],
+  matchQueue: [],
+  rows: 4,
+  cols: 5,
+  totalClickCount: 0,
+  totalFound: 0,
+  // Generate Grid for Game
+  createGameGrid: function(rows, cols) {
+    try {
+      // if rows * cols is not even number, throw error
+      if ((rows * cols) % 2 === 1) {
+        throw 'Total cards is an odd number!';
       }
-      gameGrid.push(row);
+      this.rows = rows;
+      this.cols = cols;
+      var matchValues = this.createMatchValues(this.rows * this.cols);
+      var id = 0;
+      for (var i = 0; i < this.rows * this.cols; i++) {
+        var randomResultIdx = Math.floor(Math.random() * matchValues.length);
+        this.gameGrid.push({
+          id: id,
+          matchValue: matchValues.splice(randomResultIdx, 1)[0],
+          matchedStatus: false,
+          isFlipped: false
+        });
+        id++;
+      }
+    } catch (error) {
+      console.log(error, 'Rows * Columns need to be an even number');
     }
-    return gameGrid;
-  } catch (error) {
-    console.log(error, 'Rows * Columns need to be an even number');
-  }
-}
-
-function createMatchValues(totalValues) {
-  var result = [];
-  for (var i = 0; i < totalValues / 2; i++) {
-    result.push(i);
-  }
-  return result.concat(result);
-}
-
-function consoleDisplay(gameGrid) {
-  gameGrid.forEach(function(row, rowIdx) {
-    var rowDisplayStr = '';
-    for (var col = 0; col < row.length; col++) {
-      var card = gameGrid[rowIdx][col];
-      rowDisplayStr += `[ ${card.matchValue},${card.matched} ] `;
+  },
+  // Create Reference Values for Game
+  createMatchValues: function(totalValues) {
+    var result = [];
+    for (var i = 0; i < totalValues / 2; i++) {
+      result.push(i);
     }
-    console.log(rowDisplayStr);
-  });
-}
+    return result.concat(result);
+  },
+  // Debug Display for Console
+  displayToConsole: function() {
+    var count = 0;
+    for (var i = 0; i < this.rows; i++) {
+      var rowDisplayStr = '';
+      for (var j = 0; j < this.cols; j++) {
+        var card = this.gameGrid[count];
+        rowDisplayStr += ` [id:${card.id},${card.matchValue},${
+          card.matchedStatus
+        },`;
+        rowDisplayStr += card.isFlipped === true ? `ON] ` : `OFF] `;
+        count++;
+      }
+      console.log(rowDisplayStr);
+    }
+    console.log(`Total click count: ${this.totalClickCount}`);
+    console.log(`Total found count: ${this.totalFound}`);
+  },
+  areCardsMatching: function(cardId1, cardId2) {
+    return (
+      this.gameGrid[cardId1].matchValue === this.gameGrid[cardId2].matchValue
+    );
+  },
+  activateTurn: function(id) {
+    // check that matchedStatus is false and that item is not currently in matchQueue
+    if (
+      this.gameGrid[id].matchedStatus === false &&
+      this.matchQueue.indexOf(id) === -1
+    ) {
+      this.totalClickCount++; // increase total click count
+      this.flipCard(id); // flip the card
+      this.matchQueue.push(id); // push item into matching Queue
+      // check if queue is full
+      if (this.matchQueue.length === 2) {
+        // matching Queue has 2 items. Process.
+        this.processQueue();
+      }
+    }
 
-function areCardsMatching(card1, card2) {
-  return card1.matchValue === card2.matchValue;
-}
+    if (this.totalFound === this.rows * this.cols) {
+      console.log('Game Over: All pairs found!');
+    }
+    game.displayToConsole();
+  },
+  flipCard: function(id) {
+    this.gameGrid[id].isFlipped = !this.gameGrid[id].isFlipped;
+  },
+  // Checks if the 2 Items selected are matching or need to be reset.
+  processQueue: function() {
+    var card1ID = this.matchQueue[0];
+    var card2ID = this.matchQueue[1];
+    if (this.areCardsMatching(card1ID, card2ID)) {
+      this.totalFound += 2;
+      this.gameGrid[card1ID].matchedStatus = true;
+      this.gameGrid[card2ID].matchedStatus = true;
+    } else {
+      this.flipCard(card1ID);
+      this.flipCard(card2ID);
+    }
+    this.matchQueue = [];
+  }
+};
 
-var gameGrid = createGameGrid(4, 5);
-consoleDisplay(gameGrid);
+game.createGameGrid(2, 2); // Default game is 4 rows x 5 cols;
+game.displayToConsole();

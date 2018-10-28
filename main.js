2b16ef80751710c24f9e4f30126fbb38df1bb5f5
wongco <ginson@gmail.com>
2018-10-28 03:43:48 -0700

    add logic for landing and game

--- main.js
+++ main.js
@@ -29,11 +29,62 @@
 //     save score to LocalStorage
 
 document.addEventListener('DOMContentLoaded', function() {
-  game.createGameGrid(4); // Default game is 4 rows x 5 cols;
-  game.displayToWindow();
+  landingView.start();
 });
 
-// Game Object and Methods
+var landingView = {
+  start: function() {
+    view.removeGameEventListeners();
+    var body = document.getElementById('body');
+    body.classList.remove('body-game');
+    body.classList.add('body-start');
+
+    var main = document.getElementById('main');
+    main.innerText = '';
+    main.classList.remove('main-game');
+    main.classList.add('main-start');
+
+    var newElement = document.createElement('div');
+    newElement.classList.add('header--main');
+    newElement.innerText =
+      "I'm sorry, Dave. I'm afraid I can't let you leave without playing.";
+    main.appendChild(newElement);
+
+    newElement = document.createElement('button');
+    newElement.classList.add('button--main');
+    newElement.innerText = 'Play Game';
+    main.appendChild(newElement);
+    this.setUpLandingEventListeners();
+
+    var lowScore = localStorage.getItem('lowScore');
+    if (lowScore != undefined) {
+      newElement = document.createElement('div');
+      newElement.classList.add('div--score');
+      newElement.innerText = `Record Low Score: ${lowScore}`;
+      main.appendChild(newElement);
+    }
+
+    // if localStorage exists, append Div with lowest Score to bottom of page
+  },
+  clickButton: function(event) {
+    if (event.target.className === 'button--main') {
+      startGame();
+    }
+  },
+  setUpLandingEventListeners: function() {
+    var main = document.getElementById('main');
+    // Event Listener for button
+    main.addEventListener('click', this.clickButton);
+  },
+  removeLandingEventListeners: function() {
+    var main = document.getElementById('main');
+    if (main != null) {
+      main.removeEventListener('click', this.clickButton);
+    }
+  }
+};
+
+// Game Object and Related Functions
 var game = {
   gameGrid: [],
   matchQueue: [],
@@ -41,7 +92,8 @@ var game = {
   cols: 5,
   totalClickCount: 0,
   totalFound: 0,
-  coverLetter: '🀄',
+  coverLettersArray: ['🎧', '🎮', '🎱', '🏆', '🆒'],
+  coverLetter: '',
   valueArray: [
     '🍅',
     '🍆',
@@ -60,13 +112,18 @@ var game = {
     '🍦'
   ],
   // Generate Grid for Game. If arguments are not passed default values are used to choose size
-  createGameGrid: function(rows) {
+  createGameGrid: function() {
     try {
       // if rows * cols is not even number, throw error
-      if ((rows * this.cols) % 2 === 1) {
+      if ((this.rows * this.cols) % 2 === 1) {
         throw 'Total cards is an odd number!';
       }
-      this.rows = rows;
+      // Reinitialize Values for New Game
+      this.gameGrid = [];
+      this.matchQueue = [];
+      this.totalClickCount = 0;
+      this.totalFound = 0;
+      this.coverLetter = this.coverLettersArray[Math.floor(Math.random() * 5)];
       var matchValues = this.createMatchValues(this.rows * this.cols);
       var id = 0;
       for (var i = 0; i < this.rows * this.cols; i++) {
@@ -90,83 +147,6 @@ var game = {
       result.push(i);
     }
     return result.concat(result);
-  },
-  // Debug Display for Console - Diagnostic Use Only
-  displayToConsole: function() {
-    var count = 0;
-    for (var i = 0; i < this.rows; i++) {
-      var rowDisplayStr = '';
-      for (var j = 0; j < this.cols; j++) {
-        var card = this.gameGrid[count];
-        rowDisplayStr += ` [id:${card.id},${card.matchValue},${
-          card.matchedStatus
-        },`;
-        rowDisplayStr += card.isFlipped === true ? `ON] ` : `OFF] `;
-        count++;
-      }
-      console.log(rowDisplayStr);
-    }
-    console.log(`Total click count: ${this.totalClickCount}`);
-    console.log(`Total found count: ${this.totalFound}`);
-  },
-  // Display Grid to Window
-  displayToWindow: function() {
-    var mainContainer = document.getElementById('main');
-    mainContainer.innerText = ''; // Empty Main Contents
-
-    // create clicksContainer
-    var newSection = document.createElement('section');
-    newSection.classList.add('clicksContainer');
-    newSection.setAttribute('id', 'clicksContainer');
-    mainContainer.appendChild(newSection);
-
-    // create gameContainer
-    newSection = document.createElement('section');
-    newSection.classList.add('gameContainer');
-    newSection.setAttribute('id', 'gameContainer');
-    mainContainer.appendChild(newSection);
-
-    // create foundContainer
-    newSection = document.createElement('section');
-    newSection.classList.add('foundContainer');
-    newSection.setAttribute('id', 'foundContainer');
-    mainContainer.appendChild(newSection);
-
-    // add content to clicksContainer - Clicks so Far
-    var clicksContainer = document.getElementById('clicksContainer');
-    var counterElement = document.createElement('div');
-    counterElement.innerText = `Total Clicks: ${this.totalClickCount}`;
-    clicksContainer.appendChild(counterElement);
-
-    // add content to gamesContainer - Game Content
-    var count = 0;
-    var gameContainer = document.getElementById('gameContainer');
-    for (var i = 0; i < this.rows; i++) {
-      var cardRowContainer = document.createElement('div');
-      cardRowContainer.classList.add('cardRowContainer');
-      for (var j = 0; j < this.cols; j++) {
-        var card = document.createElement('div');
-        card.classList.add('card');
-        card.setAttribute('id', this.gameGrid[count].id);
-        card.innerText = this.coverLetter;
-        cardRowContainer.appendChild(card);
-        count++;
-      }
-      gameContainer.appendChild(cardRowContainer);
-    }
-
-    // add content to foundContainer - Pairs Found
-    var foundContainer = document.getElementById('foundContainer');
-    var foundElement = document.createElement('div');
-    foundElement.innerText = `Total Found: ${this.totalFound}`;
-    foundContainer.appendChild(foundElement);
-
-    // Event Listener for cards
-    gameContainer.addEventListener('click', function clickCards(event) {
-      if (event.target.className === 'card') {
-        handlers.activateTurn(event.target.id);
-      }
-    });
   },
   // checks if the two cards are matching & returns boolean
   areCardsMatching: function(cardId1, cardId2) {
@@ -239,8 +219,142 @@ var handlers = {
     }
 
     if (game.totalFound === game.rows * game.cols) {
-      console.log('Game Over: All pairs found!');
-      // send Message to Dom and End Game
+      view.completeGame();
     }
   }
 };
+
+// View Related Functions
+var view = {
+  // Debug Display for Console - Diagnostic Use Only
+  displayToConsole: function() {
+    var count = 0;
+    for (var i = 0; i < game.rows; i++) {
+      var rowDisplayStr = '';
+      for (var j = 0; j < game.cols; j++) {
+        var card = game.gameGrid[count];
+        rowDisplayStr += ` [id:${card.id},${card.matchValue},${
+          card.matchedStatus
+        },`;
+        rowDisplayStr += card.isFlipped === true ? `ON] ` : `OFF] `;
+        count++;
+      }
+      console.log(rowDisplayStr);
+    }
+    console.log(`Total click count: ${game.totalClickCount}`);
+    console.log(`Total found count: ${game.totalFound}`);
+  },
+  // Display Grid to Window
+  displayGameToWindow: function() {
+    var body = document.getElementById('body');
+    body.classList.remove('body-start');
+    body.classList.add('body-game');
+
+    var mainContainer = document.getElementById('main');
+    mainContainer.classList.remove('main-start');
+    mainContainer.classList.add('main-game');
+    mainContainer.innerText = ''; // Empty Main Contents
+
+    // create clicksContainer
+    var newSection = document.createElement('section');
+    newSection.classList.add('clicksContainer');
+    newSection.setAttribute('id', 'clicksContainer');
+    mainContainer.appendChild(newSection);
+
+    // create gameContainer
+    newSection = document.createElement('section');
+    newSection.classList.add('gameContainer');
+    newSection.setAttribute('id', 'gameContainer');
+    mainContainer.appendChild(newSection);
+
+    // create foundContainer
+    newSection = document.createElement('section');
+    newSection.classList.add('foundContainer');
+    newSection.setAttribute('id', 'foundContainer');
+    mainContainer.appendChild(newSection);
+
+    // add content to clicksContainer - Clicks so Far
+    var clicksContainer = document.getElementById('clicksContainer');
+    var counterElement = document.createElement('div');
+    counterElement.innerText = `Total Clicks: ${game.totalClickCount}`;
+    clicksContainer.appendChild(counterElement);
+
+    // add content to gamesContainer - Game Content
+    var count = 0;
+    var gameContainer = document.getElementById('gameContainer');
+    for (var i = 0; i < game.rows; i++) {
+      var cardRowContainer = document.createElement('div');
+      cardRowContainer.classList.add('cardRowContainer');
+      for (var j = 0; j < game.cols; j++) {
+        var card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('id', game.gameGrid[count].id);
+        card.innerText = game.coverLetter;
+        cardRowContainer.appendChild(card);
+        count++;
+      }
+      gameContainer.appendChild(cardRowContainer);
+    }
+
+    // add content to foundContainer - Pairs Found
+    var foundContainer = document.getElementById('foundContainer');
+    var foundElement = document.createElement('div');
+    foundElement.innerText = `Total Found: ${game.totalFound}`;
+    foundContainer.appendChild(foundElement);
+  },
+  clickCards: function(event) {
+    if (event.target.className === 'card') {
+      handlers.activateTurn(event.target.id);
+    }
+  },
+  setUpGameEventListeners: function() {
+    var gameContainer = document.getElementById('gameContainer');
+    // Event Listener for cards
+    gameContainer.addEventListener('click', this.clickCards);
+  },
+  removeGameEventListeners: function() {
+    var gameContainer = document.getElementById('gameContainer');
+    if (gameContainer != null) {
+      gameContainer.removeEventListener('click', this.clickCards);
+    }
+  },
+  completeGame: function() {
+    var lowScore = localStorage.getItem('lowScore');
+    var gameContainer = document.getElementById('gameContainer');
+    var completeElement = document.createElement('div');
+    completeElement.classList.add('completeElement');
+    if (lowScore != undefined) {
+      if (game.totalClickCount < lowScore) {
+        localStorage.setItem('lowScore', game.totalClickCount);
+        completeElement.innerText =
+          "Congratulations! You've set a new low score!";
+      } else {
+        completeElement.innerText =
+          "Congratulations! You've completed the game!";
+      }
+    }
+    gameContainer.appendChild(completeElement);
+
+    var buttonElement = document.createElement('button');
+    buttonElement.classList.add('button--leave');
+    buttonElement.innerText = 'Return Home';
+    gameContainer.appendChild(buttonElement);
+
+    buttonElement = document.querySelector('.button--leave');
+    buttonElement.addEventListener('click', homeButton);
+
+    function homeButton(event) {
+      if (event.target.className === 'button--leave') {
+        buttonElement.removeEventListener('click', homeButton);
+        landingView.start();
+      }
+    }
+  }
+};
+
+function startGame() {
+  landingView.removeLandingEventListeners();
+  game.createGameGrid(); // Default game is 4 rows x 5 cols;
+  view.displayGameToWindow();
+  view.setUpGameEventListeners();
+}
